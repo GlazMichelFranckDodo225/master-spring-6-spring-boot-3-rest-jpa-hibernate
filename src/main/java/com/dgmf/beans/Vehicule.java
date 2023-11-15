8b6f865f586631f15236b2c6e3c5d846c95c606f
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-15 08:46:43 +0100

    Creating Beans using @Bean annotation

---
+++ src/main/java/com/dgmf/beans/Vehicule.java
@@ -0,0 +1,13 @@
+package com.dgmf.beans;
+
+public class Vehicule {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/dgmf/main/main.java
@@ -0,0 +1,26 @@
+package com.dgmf.main;
+
+import com.dgmf.beans.Vehicule;
+import com.dgmf.config.ProjectConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class main {
+    public static void main(String[] args) {
+        // Initialization of Spring IoC Container and Spring Context
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        // Fetching the Spring Beans that we want
+        var vehicule = context.getBean(Vehicule.class);
+        String helloBean = context.getBean(String.class);
+        Integer numberBean = context.getBean(Integer.class);
+
+        // Outputs
+        System.out.println("Vehicule name from the Spring Context is : "
+                + vehicule.getName());
+        System.out.println("Hello Bean says : " + helloBean);
+        System.out.println("The Value of the Number Bean is : " + numberBean);
+
+    }
+}
