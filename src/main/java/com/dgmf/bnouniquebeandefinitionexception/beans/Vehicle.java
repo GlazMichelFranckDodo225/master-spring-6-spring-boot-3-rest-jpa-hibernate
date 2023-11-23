a928e456bc436a234c490fc247f8dd69189c2871
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-23 03:56:08 +0100

    NoUniqueBeanDefinitionException

---
+++ src/main/java/com/dgmf/bnouniquebeandefinitionexception/beans/Vehicle.java
@@ -0,0 +1,13 @@
+package com.dgmf.bnouniquebeandefinitionexception.beans;
+
+public class Vehicle {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/dgmf/bnouniquebeandefinitionexception/main/main.java
@@ -0,0 +1,20 @@
+package com.dgmf.bnouniquebeandefinitionexception.main;
+
+import com.dgmf.bnouniquebeandefinitionexception.beans.Vehicle;
+import com.dgmf.bnouniquebeandefinitionexception.config.ProjectConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class main {
+    public static void main(String[] args) {
+        // Initialization of Spring IoC Container and Spring Context
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        // Fetching the Spring Beans that we want
+        var vehicle = context.getBean(Vehicle.class);
+
+        // Displaying the name of the retrieved Bean
+        System.out.println("Vehicle name from Spring Context is : " + vehicle.getName());
+    }
+}
