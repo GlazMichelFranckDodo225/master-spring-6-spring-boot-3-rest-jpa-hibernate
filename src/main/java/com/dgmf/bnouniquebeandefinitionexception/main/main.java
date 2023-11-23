86b1e5675505d3d34e84e498dfdf878aa126baa4
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-23 04:01:08 +0100

    NoUniqueBeanDefinitionException - Solution 1 - "getBean("vehicle1", Vehicle.class)"

--- src/main/java/com/dgmf/bnouniquebeandefinitionexception/main/main.java
+++ src/main/java/com/dgmf/bnouniquebeandefinitionexception/main/main.java
@@ -12,7 +12,8 @@ public class main {
                 new AnnotationConfigApplicationContext(ProjectConfig.class);
 
         // Fetching the Spring Beans that we want
-        var vehicle = context.getBean(Vehicle.class);
+        // Vehicle vehicle = context.getBean(Vehicle.class);
+        Vehicle vehicle = context.getBean("vehicle1", Vehicle.class);
 
         // Displaying the name of the retrieved Bean
         System.out.println("Vehicle name from Spring Context is : " + vehicle.getName());
