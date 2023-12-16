f1f6d2c14354fd818ca5208582b2654355a41977
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-12-16 05:50:24 +0100

    Creating Beans using XML Configurations

---
+++ src/main/java/com/dgmf/subject6/creating/beans/xml/configurations/beans/Vehicule.java
@@ -0,0 +1,17 @@
+package com.dgmf.subject6.creating.beans.xml.configurations.beans;
+
+public class Vehicule {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void vehicleMethod() {
+        System.out.println("I am a Bean of Type Vehicle");
+    }
+}
