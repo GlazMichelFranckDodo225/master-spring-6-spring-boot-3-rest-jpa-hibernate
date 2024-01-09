ce3b1f652d55a48f7748241e2e7d3e67ae01729c
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-01-09 17:21:05 +0100

    Wiring Beans using @Autowired on class fields

---
+++ src/main/java/com/dgmf/at/autowired/classfields/beans/Person.java
@@ -0,0 +1,42 @@
+package com.dgmf.at.autowired.classfields.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    private String name = "Lucy";
+    // To Autowired the Vehicle Bean at Runtime by Spring
+    // Dependency Injection
+    @Autowired
+    private Vehicle vehicle;
+
+    public Person() {
+        System.out.println("Person Bean Created by Spring ...");
+    }
+
+    public void printHello() {
+        System.out.println("Printing Hello from Component Person Bean ...");
+    }
+
+    @Override
+    public String toString() {
+        return "Person Name is : " + name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+}
